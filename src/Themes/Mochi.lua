f681be1f7e55e17f11361a2888e2d7bc8e35cd5f
createaspects <henry2565hub@gmail.com>
2024-05-09 11:35:58 -0700

    Create Mochi.lua

---
+++ src/Themes/Mochi.lua
@@ -0,0 +1,47 @@
+return {
+	Name = "Mochi",
+	Accent = Color3.fromRGB(168, 160, 50),
+
+	AcrylicMain = Color3.fromRGB(40, 40, 40),
+	AcrylicBorder = Color3.fromRGB(120, 120, 84),
+	AcrylicGradient = ColorSequence.new(Color3.fromRGB(189, 183, 107), Color3.fromRGB(252, 247, 182)),
+	AcrylicNoise = 0.92,
+
+	TitleBarLine = Color3.fromRGB(140, 139, 88),
+	Tab = Color3.fromRGB(180, 181, 141),
+
+	Element = Color3.fromRGB(198, 199, 117),
+	ElementBorder = Color3.fromRGB(101, 102, 65),
+	InElementBorder = Color3.fromRGB(117, 117, 89),
+	ElementTransparency = 0.86,
+
+	ToggleSlider = Color3.fromRGB(190, 199, 119),
+	ToggleToggled = Color3.fromRGB(0, 0, 0),
+
+	SliderRail = Color3.fromRGB(185, 186, 112),
+
+	DropdownFrame = Color3.fromRGB(201, 201, 161),
+	DropdownHolder = Color3.fromRGB(112, 109, 47),
+	DropdownBorder = Color3.fromRGB(78, 79, 35),
+	DropdownOption = Color3.fromRGB(187, 189, 113),
+
+	Keybind = Color3.fromRGB(172, 179, 109),
+
+	Input = Color3.fromRGB(193, 194, 118),
+	InputFocused = Color3.fromRGB(24, 26, 9),
+	InputIndicator = Color3.fromRGB(185, 186, 145),
+
+	Dialog = Color3.fromRGB(116, 117, 48),
+	DialogHolder = Color3.fromRGB(92, 90, 38),
+	DialogHolderLine = Color3.fromRGB(87, 84, 33),
+	DialogButton = Color3.fromRGB(120, 119, 50),
+	DialogButtonBorder = Color3.fromRGB(155, 149, 90),
+	DialogBorder = Color3.fromRGB(100, 99, 70),
+	DialogInput = Color3.fromRGB(132, 135, 55),
+	DialogInputLine = Color3.fromRGB(190, 189, 160),
+
+	Text = Color3.fromRGB(0, 0, 0),
+	SubText = Color3.fromRGB(170, 170, 170),
+	Hover = Color3.fromRGB(200, 196, 120),
+	HoverChange = 0.04,
+}
